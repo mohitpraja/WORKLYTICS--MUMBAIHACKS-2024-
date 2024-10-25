199bd21f7526589791ce6d522bd214c5d0c08cd7
Vishwajeet Shekhawat <86587242+Vish88781@users.noreply.github.com>
2024-10-25 23:50:25 +0530

    home page added

---
+++ lib/feature/splash.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:worklytics/core/colors.dart';
+import 'package:worklytics/core/fonts.dart';
+import 'package:worklytics/feature/user_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        const Duration(seconds: 2), () => Get.offAll(() => const UserView()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ColoredBox(
+        color: primaryColor,
+        child: Center(
+          child: Text(
+            'Worklytics',
+            style: TextStyle(
+                fontSize: 30,
+                letterSpacing: 1.5,
+                color: white,
+                fontFamily: FF.alata),
+          ),
+        ),
+      ),
+    );
+  }
+}
